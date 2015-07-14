ef816151e5f5da9cd49ed328eda432c018f7e419
Foxclip <destroyApp2@gmail.com>
2015-07-14 13:40:48 +1000

    packing.h, packing.c added

---
+++ packing.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "packing.h"
+
+int pack(char* filename) {
+    printf("packing file: %s\n", filename);
+}
+
+int unpack(char* filename) {
+    printf("unpacking file: %s\n", filename);
+}
---
+++ packing.h
@@ -0,0 +1,8 @@
+#ifndef PACKING_H
+#define	PACKING_H
+
+int pack(char* filename);
+int unpack(char* filename);
+
+#endif
+
