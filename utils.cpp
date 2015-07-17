b5b4c15af006ec9c52e4fdc6f2bf227695ed2af7
Foxclip <destroyApp2@gmail.com>
2015-07-17 12:28:07 +1000

    Utils added

---
+++ utils.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <cmath>
+
+void writeLongLongInt(FILE* output_file, unsigned long long d) {
+    unsigned char bytes[8];
+    for(int i = 0; i < 8; i++) {
+        unsigned long long mask = (unsigned long long)pow(2, 8*(8-i)) - (unsigned long long)pow(2, 8*(8-i-1));
+        bytes[i] = (d & mask) >> 8*(8-i-1);
+    }
+    for(int i = 0; i < 8; i++) {
+        fprintf(output_file, "%c", bytes[i]);
+    }
+}
+
+unsigned long long readLongLongInt(FILE* input_file) {
+    unsigned long long d = 0;
+    for(int i = 0; i < 8; i++) {
+        unsigned char c = getc(input_file);
+        d |= c << 8*(8-i-1);
+    }
+    return d;
+}
+
+void writeInt(FILE* output_file, int d) {
+    unsigned char bytes[4];
+    for(int i = 0; i < 4; i++) {
+        unsigned long long mask = (int)pow(2, 4*(i-4)) - (int)pow(2, 4*(i-4-1));
+        bytes[i] = (d | mask) >> (int)pow(2, 4*(i-4-1));
+    }
+    for(int i = 0; i < 4; i++) {
+        fprintf(output_file, "%c", bytes[i]);
+    }
+}
+
+unsigned long long readInt(FILE* input_file) {
+    int d = 0;
+    for(int i = 0; i < 4; i++) {
+        unsigned char c = getc(input_file);
+        d |= c << 4*(4-i-1);
+    }
+    return d;
+}
---
+++ utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define	UTILS_H
+
+void writeLongLongInt(FILE* output_file, unsigned long long d);
+unsigned long long readLongLongInt(FILE* input_file);
+void writeInt(FILE* output_file, int d);
+unsigned long long readInt(FILE* input_file);
+
+#endif
+
